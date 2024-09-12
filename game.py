8f46c5768396fd22234696f1ad4e5c1b01a180d5
Brandon Miller <brandonmiller@brandon-lee-miller-macbook-pro.lan>
2024-09-12 18:01:49 -0500

    add watch_game function to game class

--- game.py
+++ game.py
@@ -67,6 +67,81 @@ class Game:
                     self.game_id = None
 
     
-    def team_info(self):
-        '''Function to get home and away team info such as arena, city, etc...'''
-        pass
+    def watch_game(self):
+        print("Watch game")
+        game_states = {
+            "Not started": "FUT",
+            "Pregame": "PRE",
+            "Live": "LIVE",
+            "Game over": "OFF"
+        }
+
+        # r = requests.get(f"{BASE_API_URL}gamecenter/{self.game_id}/play-by-play")
+        # data = json.dumps(r.json(), indent=4)
+        # data = json.loads(data) # load json for parsing
+
+        with open("game.json", "r") as f:
+                data = json.load(f)
+
+        game_state = data["gameState"]
+        period = data["displayPeriod"]
+
+        home_score = data["homeTeam"]["score"]
+        away_score = data["awayTeam"]["score"]
+
+        while True:
+            # r = requests.get(f"{BASE_API_URL}gamecenter/{self.game_id}/play-by-play")
+            # data = json.dumps(r.json(), indent=4)
+            # data = json.loads(data) # load json for parsing
+
+            # for testing
+            with open("game.json", "r") as f:
+                data = json.load(f)
+
+            game_state = data["gameState"]
+            if game_state != "LIVE":
+                if game_state == "FUT":
+                    print("The game has not started yet. Checking again in 30 minutes.")
+                    time.sleep(1800)
+                    continue
+                elif game_state == "PRE":
+                    print("The game is about to start. Checking again in 2 minutes.")
+                    time.sleep(120)
+                    continue
+                elif game_state == "OFF": 
+                    print("The game is over.")
+                    
+                    if self.home and home_score > away_score or self.away and away_score > home_score:
+                        self.win = True
+                    else:
+                        self.win = False
+
+                    if self.win:
+                        # do victory celebration
+                        print(f"{self.team} wins!!!")
+                    else:
+                        # do sad stuff
+                        print(f"{self.team} lost. :(")
+                    
+                    break
+
+
+            period = data["displayPeriod"]
+
+            new_home_score = data["homeTeam"]["score"]
+            new_away_score = data["awayTeam"]["score"]
+
+            if new_home_score > home_score and self.home:
+                # play horn, flash lights
+                print(f"{self.team} scores!!", flush=True)
+
+            if new_away_score > away_score and self.away:
+                # play horn, flash lights
+                print(f"{self.team} scores!!", flush=True)
+
+            home_score = new_home_score
+            away_score = new_away_score
+
+            print(f"Home score is: {home_score}\nAway score is: {away_score}")
+
+            time.sleep(self.stream_delay)

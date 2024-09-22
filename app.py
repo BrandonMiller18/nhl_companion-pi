8f9aaa3c9efea37e4d6081e7d1a09a393a06c791
brandonmiller18 <blmiller0809@gmail.com>
2024-09-21 21:56:29 -0500

    add comments to code

--- app.py
+++ app.py
@@ -16,8 +16,12 @@ team_abbreviations = update_teams()
 @app.route('/')
 @app.route('/<stop_loop>')
 def index(stop_loop=0):
+    # TODO - get rid of <stop_loop> -- its stupid and unnnecessary
 
-    # check to see if there is a current game in progress, if there is we will adjust what is displayed on the homepage
+    # HACK
+    # see if there is a game happening. there might not be, thus try/except...
+    # if an error happens here, there is not a game object... so set watching to false.
+    # The "watching" variable determines page content
     try:
         if game.watching:
             watching = True
@@ -42,13 +46,16 @@ def index(stop_loop=0):
 
 @app.route('/update', methods=['POST'])
 def configure_game():
+    # handle form submission from homepage, returns redirect to homepage 
 
+    # get data fields
     user_team = request.form.get('user_team')
     stream_delay = request.form.get('stream_delay')
     enable_lights = request.form.get('lights')
     enable_audio = request.form.get('audio')
     led_count = request.form.get('led_count')
 
+    # create config.json file and write form data to file for later use
     with open('config.json', 'w') as f:
         data = {
             "user_team": user_team,
@@ -60,14 +67,22 @@ def configure_game():
 
         f.write(json.dumps(data))
 
-        flash("Configuration has been updated!", "success")
+    # show user confirmation message
+    flash("Configuration has been updated!", "success")
 
     return redirect(url_for('index'))
 
 
 @app.route('/start')
 def start_game_lp():
+    # landing page for START APP buttons. This is to prevent,
+    # as best as possible, endless loading in the user interface.
+    # however, this is not a public website, so who cares.
 
+    # HACK
+    # see if there is a game happening. there might not be, thus try/except...
+    # if an error happens here, there is not a game object... so set watching to false
+    # The "watching" variable determines page content
     try:
         if game.watching:
             watching = True
@@ -96,17 +111,23 @@ def start_game():
         enable_audio = data['enable_audio']
         led_count = data['led_count']
 
-    # instatiate game object and start the game
+    # instatiate game object and start the game.. Pass config vars to Game class
     global game 
     game = Game(user_team=user_team, stream_delay=stream_delay, enable_audio=enable_audio, enable_lights=enable_lights, led_count=led_count)
+    
+    # Get game info for user team
     game.game_info()
 
+    # if user team does not play, show error and return to homepage
     if not game.is_game:
         flash(f"{game.team} does not play today.", "danger")
         return redirect(url_for('index'))
 
+    # if you got this far, there is a game. Watch it.
     game.watch_game()
 
+    # All the game info, for some reason. I don't think its necessary at this point, but nice to have here. 
+    # NEEDS UPDATED BEFORE USED FOR ANYTHING SPECIAL
     game_info = {
         'user_team': game.team,
         'game_date': game.date,
@@ -124,9 +145,11 @@ def start_game():
         'away_score': game.away_score
     }
 
+    # Added some stop loop check here just to be safe. Don't think it actually does anything.
     if game.stop_loop:
         return redirect(url_for('end_game'))
 
+    # This template is half baked. TODO - come up with a better fucking solution
     return render_template('watch_game.html', data=game_info)
 
 
@@ -137,10 +160,19 @@ def help():
 
 @app.route('/_end-game')
 def end_game():
+    # request here means user clicked on cancel app button
+
+    # set stop_loop to true. Next time the app updates, the While True loop will break and game will end
+    # BUG - when in preview mode, it can take up to 30 min to do this, which sucks.
+    # TRY TO FIND A WAY TO MAKE THIS HAPPEN IMMEDIATELY
     game.stop_loop = True
     
+    # print out message to make sure its still working
+    i = 0
     while game.watching:
-        print("Cancelling game...", flush=True)
+        print(f"Cancelling game... {i}", flush=True)
         time.sleep(1)
+        i++
 
+    # send user back home with stop_loop path set to 1
     return redirect(url_for('index', stop_loop=1))
